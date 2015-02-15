3c83745ed327a7541939cf6f15995a9ac3b37914
Jacob Lowe <jacoblowe2.0@gmail.com>
2015-02-15 14:02:53 -0800

    adding in more filters and allowing for better quality images

--- src/svg.js
+++ src/svg.js
@@ -1,7 +1,15 @@
 var
 request = require( 'request' ),
 fs = require( 'fs' ),
-datauri = require( 'datauri' )
+datauri = require( 'datauri' ),
+gm =gm = require('gm'),
+filters = {
+    hexagon: hexFilter,
+    spectrum: multicolorFilter,
+    circle: circleFilter,
+    color: colorBurnFilter
+}
+
 
 module.exports.render = function( content, callback ) {
 
@@ -13,18 +21,47 @@ module.exports.render = function( content, callback ) {
 
 
     if( content.local ) {
-        generateURI( content.local, function( err, data ) {
-            if ( err ) return callback( err )
-            hexFilter( data, handle )
-            //multicolorFilter( data, handle )
-        } )
+        if ( !content.width && !content.height ) {
+            getImageSize( content.local, function( err, size ) {
+                if ( err ) return callback( err )
+                content.width = size.width
+                content.height = size.height
+                generateURI( content.local, applyFilter( content, handle ) )
+            } )
+            return
+            generateURI( content.local, applyFilter( content, handle ) )
+        }
         return
     }
     getSVG( content, handle )
 }
+// a thunk pass callback to execution and get a function back
+function applyFilter( content, callback ) {
+    return function( err, data ) {
+        if ( err ) return callback( err )
+        var filter = filters[ content.filter || 'hexagon' ] || filters.hexagon
+        filter( {
+            datauri: data,
+            width: content.width,
+            height: content.height,
+            color: content.color
+        }, callback )
+    }
+}
+
+function getImageSize( url, callback ) {
+    // obtain the size of an image
+    gm( url )
+        .options({imageMagick: true})
+        .size( callback )
+}
 
 function getSVG( content, callback ) {
     var file = 'tmp/' + content.session + '.jpg'
+
+    function done( ) {
+        generateURI( file, applyFilter( content, callback ), true )
+    }
 
     // downloads photo
     request( content.photo )
@@ -32,14 +69,18 @@ function getSVG( content, callback ) {
         .pipe( fs.createWriteStream( file ) )
         .on( 'error', callback )
         .on( 'close', function() {
-            
-            generateURI( file, function( err, data ) {
-                if ( err ) return callback( err );
-                //hexFilter( data, callback )
-                multicolorFilter( data, callback )
-            }, true )
+            if ( !content.width && !content.height ) {
+                getImageSize( file, function( err, size ) {
+                    if ( err ) return callback( err )
+                    console.log( size )
+                    content.width = size.width
+                    content.height = size.height
+                    done()
+                } )
+                return
+                done()
+            }
 
-            
         } )
 }
 
@@ -53,11 +94,12 @@ function generateURI( file, callback, unlink ) {
     } )
 }
 
-function hexFilter( bgImage, callback ) {
+function hexFilter( content, callback ) {
     var
     vsvg = require( 'vsvg' ),
     paths = require( 'vsvg-paths' ),
-    size = 400,
+    width = content.width || 400,
+    height = content.height || 400,
     svg = vsvg.svg,
     path = vsvg.path,
     defs = vsvg.defs,
@@ -71,13 +113,6 @@ function hexFilter( bgImage, callback ) {
     feColorMatrix = vsvg.feColorMatrix,
     clipPath = vsvg.clipPath,
     hexPath = [
-        // { x: 0, y: 200 },
-        // { x: 100, y: 25 },
-        // { x: 300, y: 25 },
-        // { x: 400, y: 200 },
-        // { x: 300, y: 375 },
-        // { x: 100, y: 375 },
-        // {}
         { x: 20, y: 200 },
         { x: 115, y: 45 },
         { x: 285, y: 45 },
@@ -96,12 +131,12 @@ function hexFilter( bgImage, callback ) {
         { x: 125, y: 335 },
         { x: 40, y: 200 },
         {}
-    ];
+    ]
     callback( null, (
-        svg( { xmlns: 'http://www.w3.org/2000/svg', version: '1.1', width: size, height: size, 'shape-rendering': 'crispEdges', 'xmlns:xlink': 'http://www.w3.org/1999/xlink' },
+        svg( { xmlns: 'http://www.w3.org/2000/svg', version: '1.1', width: width, height: height, 'shape-rendering': 'crispEdges', 'xmlns:xlink': 'http://www.w3.org/1999/xlink' },
             defs( {},
                 pattern( { id: 'bg-image', patternUnits: 'userSpaceOnUse', width: '100%', height: '100%' },
-                    image( { 'xlink:href': bgImage, width: '100%', height: '100%', x: 0, y: 0 } )
+                    image( { 'xlink:href': content.datauri, width: '100%', height: '100%', x: 0, y: 0 } )
                 ),
                 clipPath( { id: 'hex-clip' },
                     path( { d: paths.encode( hexPath ) } )
@@ -145,7 +180,116 @@ function hexFilter( bgImage, callback ) {
     ) )
 }
 
-function multicolorFilter( bgImage, callback ) {
+function colorBurnFilter( content, callback ) {
+    var
+    vsvg = require( 'vsvg' ),
+    paths = require( 'vsvg-paths' ),
+    width = content.width || 400,
+    height = content.height || 400,
+    size = content.width > content.height ? content.height : content.width, // small dimension
+    svg = vsvg.svg,
+    defs = vsvg.defs,
+    color = content.color || 'papayawhip',
+    pattern = vsvg.pattern,
+    image = vsvg.image,
+    rect = vsvg.rect,
+    filter = vsvg.filter,
+    feMerge = vsvg.feMerge,
+    feMergeNode = vsvg.feMergeNode,
+    feFlood = vsvg.feFlood,
+    feColorMatrix = vsvg.feColorMatrix,
+    clipPath = vsvg.clipPath;
+    callback( null, (
+        svg( { xmlns: 'http://www.w3.org/2000/svg', version: '1.1', width: width, height: height, 'shape-rendering': 'crispEdges', 'xmlns:xlink': 'http://www.w3.org/1999/xlink' },
+            defs( {},
+                pattern( { id: 'bg-image', patternUnits: 'userSpaceOnUse', width: '100%', height: '100%' },
+                    image( { 'xlink:href': content.datauri, width: '100%', height: '100%', x: 0, y: 0 } )
+                ),
+                filter( { className: 'colorFlood', id: 'rect-filter', x: '0%', y: '0%', width: '100%', height: '100%' },
+                    feFlood( { 'flood-color': color, result: 'A' } ),
+                    feColorMatrix( { type: 'matrix', in: 'SourceGraphic', result: 'B', values: '1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 1 1 1 0 0' } ),
+                    feMerge( {},
+                        feMergeNode( { in: 'A' } ),
+                        feMergeNode( { in: 'B' } )
+                    )
+                )
+            ),
+            rect( { width: '100%', height: '100%', x: 0, y: 0, fill: 'url(#bg-image)', filter: 'url(#rect-filter)' } )
+        )
+    ) )
+}
+
+
+function circleFilter( content, callback ) {
+    var
+    vsvg = require( 'vsvg' ),
+    paths = require( 'vsvg-paths' ),
+    width = content.width || 400,
+    height = content.height || 400,
+    size = content.width > content.height ? content.height : content.width, // small dimension
+    svg = vsvg.svg,
+    circle = vsvg.circle,
+    defs = vsvg.defs,
+    color = content.color || 'papayawhip',
+    pattern = vsvg.pattern,
+    image = vsvg.image,
+    rect = vsvg.rect,
+    filter = vsvg.filter,
+    feMerge = vsvg.feMerge,
+    feMergeNode = vsvg.feMergeNode,
+    feFlood = vsvg.feFlood,
+    feColorMatrix = vsvg.feColorMatrix,
+    clipPath = vsvg.clipPath;
+    callback( null, (
+        svg( { xmlns: 'http://www.w3.org/2000/svg', version: '1.1', width: width, height: height, 'shape-rendering': 'crispEdges', 'xmlns:xlink': 'http://www.w3.org/1999/xlink' },
+            defs( {},
+                pattern( { id: 'bg-image', patternUnits: 'userSpaceOnUse', width: '100%', height: '100%' },
+                    image( { 'xlink:href': content.datauri, width: '100%', height: '100%', x: 0, y: 0 } )
+                ),
+                clipPath( { id: 'circle-clip' },
+                    circle( { cx: width / 2, cy: height / 2, r: size / 2 })
+                ),
+                clipPath( { id: 'inside-circle-clip' },
+                    circle( { cx: width / 2, cy: height / 2, r: size / 3 })
+                ),
+                filter( { className: 'colorFlood', id: 'circle-filter', x: '0%', y: '0%', width: '100%', height: '100%' },
+                    feFlood( { 'flood-color': 'black', result: 'A' } ),
+                    feColorMatrix( { type: 'matrix', in: 'SourceGraphic', result: 'B', values: '1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 1 1 1 0 0' } ),
+                    feMerge( {},
+                        feMergeNode( { in: 'A' } ),
+                        feMergeNode( { in: 'B' } )
+                    )
+                ),
+                filter( { className: 'colorFlood', id: 'circle-filter2', x: '0%', y: '0%', width: '100%', height: '100%' },
+                    feFlood( { 'flood-color': color, result: 'A' } ),
+                    feColorMatrix( { type: 'matrix', in: 'SourceGraphic', result: 'B', values: '1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 1 1 1 0 0' } ),
+                    feMerge( {},
+                        feMergeNode( { in: 'A' } ),
+                        feMergeNode( { in: 'B' } )
+                    )
+                ),
+                filter( { className: 'colorFlood', id: 'circle-filter3', x: '0%', y: '0%', width: '100%', height: '100%' },
+                    feFlood( { 'flood-color': 'black', result: 'A' } ),
+                    feColorMatrix( { type: 'matrix', in: 'SourceGraphic', result: 'B', values: '1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 1 1 1 0 0' } ),
+                    feMerge( {},
+                        feMergeNode( { in: 'A' } ),
+                        feMergeNode( { in: 'B' } )
+                    )
+                )
+            ),
+            rect( { width: '100%', height: '100%', x: 0, y: 0, fill: 'url(#bg-image)', filter: 'url(#circle-filter)' } ),
+            rect( { width: '100%', height: '100%', x: 0, y: 0, fill: 'url(#bg-image)', filter: 'url(#circle-filter2)', style: { 
+                'clip-path': 'url(#circle-clip)' 
+            } } ),
+            rect( { width: '100%', height: '100%', x: 0, y: 0, fill: 'url(#bg-image)' , filter: 'url(#circle-filter3)', style: { 
+                'clip-path': 'url(#inside-circle-clip)' 
+            } } )
+        )
+    ) )
+}
+
+
+function multicolorFilter( content, callback ) {
     var
     vsvg = require( 'vsvg' ),
     PerlinNoise = require( 'perlin-simplex' ),
@@ -154,16 +298,19 @@ function multicolorFilter( bgImage, callback ) {
     fs = require( 'fs' ),
     nRows = 15,
     nCols = nRows,
+    width = content.width || 400,
+    height = content.height || 400
     size = 400,
     noiseDistance = 20,
     zNoiseDistance = 1000,
     colorDistance = 0.5,
-    ratio = size / nRows,
+    ratiox = width / nRows,
+    ratioy = height / nRows,
     svg = vsvg.svg({
         xmlns: 'http://www.w3.org/2000/svg',
         version: '1.1',
-        width: size,
-        height: size,
+        width: width,
+        height: height,
         'shape-rendering': 'crispEdges'
     }),
     defs = vsvg.defs( ),
@@ -174,7 +321,7 @@ function multicolorFilter( bgImage, callback ) {
         height: '100%',
     })
     image = vsvg.image({
-        'xlink:href': bgImage,
+        'xlink:href': content.datauri,
         width: '100%',
         height: '100%',
         x: 0,
@@ -183,8 +330,8 @@ function multicolorFilter( bgImage, callback ) {
     bg = vsvg.rect({
         x: 0,
         y: 0,
-        width: size,
-        height: size,
+        width: width,
+        height: height,
         fill: 'url(#bg-image)'
     })
 
@@ -248,10 +395,10 @@ function multicolorFilter( bgImage, callback ) {
                 in: 'B'
             }),
             rect = vsvg.rect({
-                x: ( column * ratio ),
-                y: ( row * ratio ),
-                width: ratio,
-                height: ratio,
+                x: ( column * ratiox ),
+                y: ( row * ratioy ),
+                width: ratiox,
+                height: ratioy,
                 filter: 'url(#' + id + ')',
                 fill: 'url(#bg-image)'
             })
